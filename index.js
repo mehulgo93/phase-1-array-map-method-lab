c324b8c7338e418dddeff1d694bff0c4b84a019a
Mehul <mehul_angle@hotmail.com>
2023-06-20 21:09:55 -0400

    Map Iterator Method

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map(tutorial => {
+      const words = tutorial.split(" ")
+      const titleCasedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+       const titleCasedTutorials = titleCasedWords.join(' ')
+       return titleCasedTutorials;
+    })
+
 }
